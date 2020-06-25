4dfbbe11c1eb0b1bdb4c34312c7c3883f459bd52
msrahman <md_shahriar_rahman@sfu.ca>
2020-06-25 04:25:07 -0700

    added home, campgrounds, partials, app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+//const { urlencoded } = require("express");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended : true}));
+
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+]
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds.ejs", {camps : data});
+});
+
+app.get("/campgrounds/new", (req, res) =>{
+    res.render("new.ejs");
+});
+app.post("/campgrounds", (req, res) => {
+    //console.log(req.body);
+    data.push({name: req.body.name, image: req.body.image});
+    res.redirect("/campgrounds");
+});
+
+app.listen(3000, function(){
+    console.log("server started");
+});
