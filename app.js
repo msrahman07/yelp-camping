7020f8fdb05d8d395b2d43a7b7effb93416b30be
msrahman <msrahman@sfu.ca>
2020-07-23 00:26:25 -0700

    added custom css file and changed some styling on show page figures

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require("express"),
 //const { urlencoded } = require("express");
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
-
+app.use(express.static(__dirname + '/public'));
 //database mongodb connection
 // const MongoClient = require('mongodb').MongoClient;
 // const ObjectId = require('mongodb');
